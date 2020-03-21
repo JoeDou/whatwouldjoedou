51e619c771caa4dadc189c506dbfa41778b9b64a
Joe Dou <joe.dou@gmail.com>
2020-03-21 14:51:13 -0700

    use create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import underConstruction from './under_construction.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={underConstruction} className="App-logo" alt="underConstruction" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
