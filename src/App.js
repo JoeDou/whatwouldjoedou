998a715929103840e7d33f0cb9f97d064dcdf4f2
Joe Dou <joe.dou@gmail.com>
2020-03-21 15:22:57 -0700

    Update title, links and favicon

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={underConstruction} className="App-logo" alt="underConstruction" />
+        <a
+          className="App-link"
+          href="https://whatwouldjoedou.azurewebsites.net/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Famo-space
+        </a>
+        <a
+          className="App-link"
+          href="https://http://joedou.github.io/sudoku//"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Sudoku
+        </a>
       </header>
     </div>
   );
