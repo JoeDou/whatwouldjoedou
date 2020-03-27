eeb4b0cbd29bef06600851f5e7790879a39dbb65
Joe Dou <joe.dou@gmail.com>
2020-03-27 12:29:10 -0700

    remove comments, unused css and files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Home from "./home"
-import Projects from "./projects"
-import Profile from "./profile"
 import { Switch, Route, Redirect } from "react-router-dom";
 import { Navbar, Nav } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import Home from "./home"
+import Projects from "./projects"
+import Profile from "./profile"
 
 function App() {
   return (
@@ -58,46 +58,5 @@ function App() {
     </div>
   );
 }
-
-// function Profile() {
-//   return (
-//     <Container>
-//       <h1 className="profile-title">Profile</h1>
-//       <h4 className="profile-subtext">Hands on engineering leader with experience leading full stack teams</h4>
-//       <Row>
-//         <Col lg={6} md={12}>
-//           <Image
-//             src={process.env.PUBLIC_URL + "Joe-Dou.jpg"}
-//             fluid
-//           />
-//         </Col>
-//         <Col lg={6} md={12}>
-//           <h2>Details</h2>
-//           <p><strong>Name:</strong><span>&nbsp;Joe Dou</span></p>
-//           <p><strong>Location:</strong><span>&nbsp;SF Bay Area</span></p>
-//           <p><strong>Technical Skill:</strong><span>&nbsp;JavaScript, ReactJS, Redux, Webpack, Backbone, Styled Components, CSS, HTML, Ruby, Grape, NodeJS, Express, Python, C++, MongoDB, Postgres, Redis, Docker, AWS, D3, AngularJS, Rails, Kubernetes</span></p>
-//           <div className="logo-wrapper">
-//             <a className="logo" href="https://www.linkedin.com/in/JoeDou" rel="noopener noreferrer" target="_blank" >
-//               <img
-//                 width={64}
-//                 height={64}
-//                 src={process.env.PUBLIC_URL + "linkedin.png"}
-//                 alt="Generic placeholder"
-//               />
-//             </a>
-//             <a className="logo" href="https://www.github.com/JoeDou " rel="noopener noreferrer" target="_blank" >
-//               <img
-//                 width={64}
-//                 height={64}
-//                 src={process.env.PUBLIC_URL + "github.png"}
-//                 alt="Generic placeholder"
-//               />
-//             </a>
-//           </div>
-//         </Col>
-//       </Row>
-//     </Container>
-//   )
-// }
 
 export default App;
